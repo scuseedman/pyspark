6828a2a8320c09f1eef5f1c95c7ffdb5694d5593
seed <liwangdong@jrq.com>
2017-12-05 16:34:08 +0800

    过滤日志文件，计算需要的指标

---
+++ com/seed/log-analy/__init__.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# -*- coding:utf8 -*-
+
+"""
+@version: ??
+@author: lwd
+@license: Apache Licence 
+@contact: scuseedman@163.com
+@site: http://www.alaxigaodi.com
+@software: PyCharm
+@file: __init__.py.py
+@time: 2017/12/5 14:15
+
+# code is far away from bugs with the god animal protecting
+    I love animals. They taste delicious.
+              ┏┓      ┏┓
+            ┏┛┻━━━┛┻┓
+            ┃      ☃      ┃
+            ┃  ┳┛  ┗┳  ┃
+            ┃      ┻      ┃
+            ┗━┓      ┏━┛
+                ┃      ┗━━━┓
+                ┃  神兽无影    ┣┓
+                ┃　BUG无踪！   ┏┛
+                ┗┓┓┏━┳┓┏┛
+                  ┃┫┫  ┃┫┫
+                  ┗┻┛  ┗┻┛
+"""
+import os
+
+os.environ["SPARK_HOME"] = "D:\soft\spark-1.6.0-bin-hadoop2.6"
+
+if __name__ == '__main__':
+    pass
