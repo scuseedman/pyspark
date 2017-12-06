5655e2b42bbeab5b0bbe81ae117046ef70556daa
seed <liwangdong@jrq.com>
2017-12-06 16:01:28 +0800

    partner_mobile数据解析

--- com/seed/parnter-mobile/MobileAnaly.py
+++ com/seed/parnter-mobile/MobileAnaly.py
@@ -40,14 +40,8 @@ os.environ["SPARK_HOME"] = "D:\soft\spark-1.6.0-bin-hadoop2.6"
 #处理partner文件
 def partneranaly(partnerfile):
     partners = sc.textFile(partnerfile)
-    for y in partners.collect():
-        print(y)
-    print("................ 第一个分割线 ..........................")
     rows = partners.map(lambda line:line.split("|"))
         # .map(lambda word: Row(partner_no=word[0], city_name=word[1], area_name=word[2],sc_pid=word[3],sc_id=word[4],comp_addr=word[5]))  # 不能这样处理，加了这个数据格式会错乱
-    for x in rows.collect():
-        print(x)
-    print("................ 第二个分割线 ..........................")
     fields = []
     fields.append(StructField("partner_no", StringType(), True))
     fields.append(StructField("city_name", StringType(), True))
@@ -66,13 +60,7 @@ def partneranaly(partnerfile):
 #处理mobile
 def mobileanaly(mobilefile):
     mobiles = sc.textFile(mobilefile)
-    for y in mobiles.collect():
-        print(y)
-    print("................ 第一个分割线 ..........................")
     rows = mobiles.map(lambda line: line.split("\t"))
-    for x in rows.collect():
-        print(x)
-    print("................ 第二个分割线 ..........................")
     fields = []
     fields.append(StructField("partner_no", StringType(), True))
     fields.append(StructField("comp_no", StringType(), True))
@@ -93,19 +81,35 @@ def mobileleftjoinpartner(p_df,m_df):
     # for x in rows.collect:
     #     print(x)
     print(rows.show())
+    return rows
+
+
+# 数据结果处理，待看文档
+def savedfastext(rows):
+    print("........................... 数据结果格式化")
+    print(rows.columns)
+    print(rows.describe(["city_name","mobile"]).show())
+    # rows.select("mobile").write.format("text").mode("append").save("joinres.txt")
+    # rows.write.save("namesAndAges.parquet", format="parquet")       #存储为目录下的文件，格式为parquet
+    prop = {"user":"root","password":"hadoop","driver":"com.mysql.jdbc.Driver"}
+    rows.write.format("jdbc").jdbc("jdbc:mysql://hadoop03:3306/test","t_spark_dataframe_test",prop) # // 表可以不存在,数据库连接失败 seed
+
+"""
+The solution is to add an environment variable named as "PYSPARK_SUBMIT_ARGS" and set its value to
+"--packages com.databricks:spark-csv_2.10:1.4.0 pyspark-shell".It will work fine.
+"""
 
 if __name__ == '__main__':
     sqlContext = conf = SparkConf().setAppName("log analy")
     sc = SparkContext(conf=conf)
     sqlContext = SQLContext(sc)
     sc.setLogLevel("WARN")
-
     partnerfile = "../test/partner_city_area"
     partner_df = partneranaly(partnerfile)
 
     mobilefile = "../test/mobile_demo.txt"
     mobile_df = mobileanaly(mobilefile)
     print("....................... 表开始关联 .........................")
-    mobileleftjoinpartner(partner_df, mobile_df)
-
+    rows = mobileleftjoinpartner(partner_df, mobile_df)
+    savedfastext(rows)
     sc.stop()
